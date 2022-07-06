fab96a8023b87d4494c13c3359225cc8f4141e01
vjhas95 <108676565+vjhas95@users.noreply.github.com>
2022-07-07 02:03:56 +0530

    Add files via upload
    Modified JS

--- main.js
+++ main.js
@@ -1,10 +1,61 @@
+
 var input1 = " ";
 var input2 = " ";
 var pressedOp;
-var digitInput = document.querySelector("#digit-input");
-
+var digitInput = document.querySelector("input");
 var buttons = document.querySelectorAll(".btn");
 
+for (let i = 0; i < buttons.length; i++) {
+    
+    buttons[i].addEventListener("click" , function(){
+        let buttonText = this.innerHTML;
+        //Checking for buttons 0-9, '.' 
+        if ((buttonText >= '0' && buttonText <= '9') || buttonText == '.') {
+                digitInput.value = getInput(buttonText);
+        }
+        // Checking for *,/,+,-
+        else if (buttonText == '*' || buttonText == '/' || buttonText == '+' || buttonText == '-') {
+            /* if input1 and input2 have some value and also pressedOp has some operator,
+               then calculate and store result in input1 and clear input2.*/
+            if(input1 != " " && input2 != " "){
+                digitInput.value = input1 = calculate(pressedOp);
+                input2 = " "; 
+                pressedOp = buttonText;
+                operatorBgChange(buttons[i]);
+            }
+            /* if input1 has some value and operator is pressed.*/ 
+            else{
+                pressedOp = buttonText;
+                operatorBgChange(buttons[i]);
+            }
+
+        }
+        // Checking for '='
+        else if(buttonText == "="){   
+            // Evaluate the expression according to operator button pressed             
+            digitInput.value = calculate(pressedOp);  
+        }
+        else if(buttonText == 'Clear'){
+            digitInput.value = 0;
+            clear();
+        }  
+        else{
+            digitInput.value = "Invalid Expression";
+        }     
+    });
+}
+function getInput(btnText){
+    // getting operand1 until any operator button is pressed.
+    if(pressedOp == undefined){
+        input1 = input1 + btnText;
+        return input1;    
+    }
+    else{
+    //getting operand2 after any operator button is pressed.
+        input2 = input2 + btnText;
+        return input2;
+    }
+}
 function calculate(op){
         let result;
         switch (op) {
@@ -25,50 +76,15 @@ function calculate(op){
         }
         return result;
 }
+function operatorBgChange(btnObject){
+    btnObject.classList.add("pressed-op");
+    //digitInput.value = pressedOp;
+    setTimeout(() => {
+        btnObject.classList.remove("pressed-op");
+    }, 1000);
+}
 function clear(){
     pressedOp = undefined;
     input1 = " ";
     input2 = " ";
 }
-
-for (let i = 0; i < buttons.length; i++) {
-    
-    buttons[i].addEventListener("click" , function(){
-        let buttonText = this.innerHTML;
-        //Listening for buttons 0-9, '.' 
-        if ((buttonText >= '0' && buttonText <= '9') || buttonText == '.') {
-            // getting operand1 until any operator button is pressed.
-            if(pressedOp == undefined){
-                digitInput.value = input1 = input1 + buttonText;
-            }
-            else{
-            //getting operand2 after any operator button is pressed.
-                digitInput.value = input2 = input2 + buttonText;      
-            }
-        } 
-        // Checking for *,/,+,-,=
-        if (buttonText == '*' || buttonText == '/' || buttonText == '+' || buttonText == '-') {
-            pressedOp = buttonText;
-            buttons[i].classList.add("pressed-op");
-            digitInput.value = pressedOp;
-            setTimeout(() => {
-                buttons[i].classList.remove("pressed-op");
-            }, 2000);
-
-            
-        }else if(buttonText == "="){   
-            // Evaluate the expression according to operator button pressed             
-            digitInput.value = calculate(pressedOp);
-            clear();   
-        }
-        if(buttonText == 'Clear'){
-            digitInput.value = 0;
-            clear();
-        }       
-    });
-}
-
-
-
-
-
